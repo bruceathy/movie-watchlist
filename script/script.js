f285f725ac984a9b0ecbf85ef05b620a3ca9e741
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-01-27 21:14:55 -0500

    update

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const apiKey = "2880ab2f&";
 const search = document.getElementById("search");
 const searchBtn = document.getElementById("search-btn");
+const savedMovies = document.getElementById("watchlist");
 let watchList = [];
 
 function fetchMovies(e) {
@@ -8,12 +9,7 @@ function fetchMovies(e) {
   let movieEl = "";
 
   fetch(`http://www.omdbapi.com/?apikey=${apiKey}s=${search.value}`)
-    .then((res) => {
-      if (!res.ok) {
-        throw Error("Weather data not available");
-      }
-      return res.json();
-    })
+    .then((res) => res.json())
     .then((data) => {
       for (let movie of data.Search) {
         fetch(`http://www.omdbapi.com/?apikey=${apiKey}i=${movie.imdbID}`)
@@ -48,38 +44,39 @@ function addToWatchlist(movie) {
   watchList.push(movie);
   localStorage.setItem("watchlist-item", JSON.stringify(watchList));
   console.log(localStorage.getItem("watchlist-item"));
-  renderWatchlist(watchList);
+  // renderWatchlist(watchList);
+  // console.log(watchList);
 }
 
 // PUT EVERY WATCHLIST MOVIE IN AN ARRAY THEN USE MAP TO CREATE AN HTML ELEMENT FOR
 // EACH ITEM AND APPEND IT TO THE WATCHLIST CONTAINER
-function renderWatchlist(list) {
-  let watchlistEl = "";
-  // document.getElementById("watchlist").style.display = "none";
+// function renderWatchlist(list) {
+//   let watchlistEl = "";
+//   // document.getElementById("watchlist").style.display = "none";
 
-  for (let i = 0; i < list.length; i++) {
-    fetch(`http://www.omdbapi.com/?apikey=${apiKey}i=${list[i]}`)
-      .then((res) => res.json())
-      .then((data) => {
-        watchlistEl += `
-            <div class="movie">
-              <div class="poster" style="background-image: url('${data.Poster}'); background-repeat: no-repeat; background-size: cover; background-size: 100% 100%;">
-                <div class="movie-plot">
-                  <p>${data.Plot}</p>
-              </div>
-              </div>
-              <div class="movie-info">
-                <h3>${data.Title}</h3>
-                <p><strong>Year: </strong>${data.Year}</p>
-                <p><strong>Type: </strong>${data.Type}</p>
-                <p><strong>Rating: </strong>${data.Rated}</p>
-                <p class="watchlist-btn" id="remove-btn" onclick="removeFromWatchlist('${list[i]}')"><a>Remove</a></p>
-              </div>
-            </div>
-              `;
-        document.getElementById("saved").innerHTML = watchlistEl;
-      });
-  }
-}
+//   for (let i = 0; i < list.length; i++) {
+//     fetch(`http://www.omdbapi.com/?apikey=${apiKey}i=${list[i]}`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         watchlistEl += `
+//             <div class="movie">
+//               <div class="poster" style="background-image: url('${data.Poster}'); background-repeat: no-repeat; background-size: cover; background-size: 100% 100%;">
+//                 <div class="movie-plot">
+//                   <p>${data.Plot}</p>
+//               </div>
+//               </div>
+//               <div class="movie-info">
+//                 <h3>${data.Title}</h3>
+//                 <p><strong>Year: </strong>${data.Year}</p>
+//                 <p><strong>Type: </strong>${data.Type}</p>
+//                 <p><strong>Rating: </strong>${data.Rated}</p>
+//                 <p class="watchlist-btn" id="remove-btn" onclick="removeFromWatchlist('${list[i]}')"><a>Remove</a></p>
+//               </div>
+//             </div>
+//               `;
+//         document.getElementById("saved").innerHTML = watchlistEl;
+//       });
+//   }
+// }
 
 searchBtn.addEventListener("click", fetchMovies);
